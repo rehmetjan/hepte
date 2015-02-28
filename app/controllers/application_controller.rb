6a396566336b1171efa9ad541a63d572d49b8e76
yasinn <303620016@qq.com>
2015-02-28 09:53:15 +0800

    login & logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,24 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  helper_method :login?, :current_user
+  
+  def login?
+    !!current_user
+  end
+  
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+    @current_user
+  end
+  
+  def login_as(user)
+    session[:user_id] = user.id
+    @current_user = user
+  end
+  
+  def logout
+    session.delete(:user_id)
+    @current_user = nil
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,17 +3,24 @@ class SessionsController < ApplicationController
   end
   
   def create
-    @user = User.find login_params
+    @user = User.find_by_email login_params[:email]
     if @user && @user.authenticate(login_params[:password])
+      login_as @user
       redirect_to root_url
     else
-      flash.now[:warning] = 'incorrect password or email'
+      flash.now[:warning] = "incorrect password or email"
+      render :new
     end
+  end
+  
+  def destroy
+    logout
+    redirect_to root_url
   end
   
   private
   
   def login_params
-    params.require(:user).permit(:email, :password)
+    params.require(:session).permit(:email, :password)
   end
 end
