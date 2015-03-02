afcd98f3633ea64e1fa4700ae6cbe7b0cf4e157e
yasinn <303620016@qq.com>
2015-03-02 10:03:39 +0800

    signup confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,4 +22,19 @@ class User < ActiveRecord::Base
     nil
   end
   
+  def confirm
+    update_attribute :confirmed, true
+  end
+
+  def confirmation_token
+    self.class.verifier_for('confirmation').generate([id, email, Time.now])
+  end
+
+  def self.find_by_confirmation_token(token)
+    user_id, email, timestamp = verifier_for('confirmation').verify(token)
+    User.find_by(id: user_id, email: email) if timestamp > 1.hour.ago
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    nil
+  end
+  
 end
