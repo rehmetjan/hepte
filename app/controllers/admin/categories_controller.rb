df0016db2e0aff3a1ddcd09a90968c55d864fdd1
yasinn <303620016@qq.com>
2015-03-13 14:25:43 +0800

    categories controller crud

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,42 @@
-class Admin::CategoriesController < ApplicationController
+class Admin::CategoriesController < Admin::ApplicationController
+  
+  def index
+    @category = Category.order(books_count: :desc)
+  end
+  
+  def show
+  end
+  
+  def new
+    @category = Category.new
+  end
+  
+  def create
+    @category = Category.new category_params
+  end
+  
+  def update
+    if @category.update_attributes category_params
+      flash[:success] = I18n.t('admin.categories.flashes.successfully_updated')
+      redirect_to admin_category_path(@category)
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @category.destroy
+    flash[:success] = I18n.t('admin.categories.flashes.successfully_destroy')
+    redirect_to admin_categories_path
+  end
+  
+  private
+  
+  def category_params
+    params.require(:category).permit(:name, :slug)
+  end
+  
+  def find_category
+    @category = Category.find params[:id]
+  end
 end
